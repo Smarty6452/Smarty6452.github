880dfe421594077e9cf566f9947ed7b897979d57
Smarty <rohitbharti326452@gmail.com>
2021-09-21 14:56:26 +0530

    Update App.js

--- login-form/src/App.js
+++ login-form/src/App.js
@@ -5,9 +5,16 @@ import './App.css';
 import { Button, Form, FormGroup, label, Input } from 'reactstrap';
 
 import { FacebookLoginButton } from 'react-social-login-buttons';
+import Navbar from './components/Navbar';
 
 class App extends Component {
 	render() {
+		// function to opening url
+		const openInNewTab = (url) => {
+			const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+			if (newWindow) newWindow.opener = null
+		}
+	
 		return (
 			
 			<Form className="login-form">
@@ -32,11 +39,11 @@ class App extends Component {
 				<br />
 
 				{/* <button className = "btn-lg btn-dark btn-block">Log in </button> */}
-				<button type="button" className="btn btn-secondary">
+				<button type="button"  className="btn btn-secondary">
 					Log in
 				</button>
 				<div className="text-center pt-3">Or continue with your social account</div>
-				<FacebookLoginButton className="mt-3 mb-3" />
+				<FacebookLoginButton onClick={() => openInNewTab('https://www.facebook.com/')} className="mt-3 mb-3" />
 				<div className="text-center">
 					<a href="/signup">Sign up</a>
 					<span className="p-2">|</span>
